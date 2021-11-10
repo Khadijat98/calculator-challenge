4089887f4f1ceaff8aaa22d88e04fe8c2f8c52bc
Khadijat Oyeleye <kaydee98@hotmail.co.uk>
2021-11-10 12:47:36 +0000

    added full functionality

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const numberKeys = document.querySelectorAll(".number")
 const operatorKeys = document.querySelectorAll(".operator")
 const equalsKey = document.querySelector("#equals")
 const clearKey = document.querySelector("#clear")
+const percentageKey = document.querySelector("#percentage")
+const plusMinusKey = document.querySelector("#positive-or-negative")
 
+// function for the numbers and the decimal point
 numberKeys.forEach((numberKey) => {
   numberKey.addEventListener("click", (event) => {
     if (calcDisplay.innerHTML==operator) {
@@ -14,10 +17,13 @@ numberKeys.forEach((numberKey) => {
   })
 });
 
+// variables that will be updated
 let firstNum;
 let secondNum;
 let operator;
 
+
+// function for the operators
 operatorKeys.forEach((operatorKey) => {
   operatorKey.addEventListener("click", (event) => {
   operator = event.target.innerHTML;
@@ -26,24 +32,43 @@ operatorKeys.forEach((operatorKey) => {
   })
 })
 
+// function for percentage
+percentageKey.addEventListener("click", () => {
+  let percentageValue = calcDisplay.innerHTML;
+  calcDisplay.innerHTML = percentageValue / 100;
+  if (percentageValue == false) {
+    calcDisplay.innerHTML = "";
+  }
+})
+
+// function for +/-
+plusMinusKey.addEventListener("click", () => {
+  let plusMinusValue = calcDisplay.innerHTML;
+  calcDisplay.innerHTML = plusMinusValue * -1;
+  if (plusMinusValue == false) {
+    calcDisplay.innerHTML = "";
+  }
+})
+
+// function for equals
 equalsKey.addEventListener("click", ()  => {
  secondNum = calcDisplay.innerHTML;
  if (operator == "+") {
-  calcDisplay.innerHTML = Number(firstNum) + Number(secondNum);
+  calcDisplay.innerHTML = parseFloat(firstNum) + parseFloat(secondNum);
  } else if (operator == "-") {
-  calcDisplay.innerHTML = Number(firstNum) - Number(secondNum);
+  calcDisplay.innerHTML = parseFloat(firstNum) - parseFloat(secondNum);
  } else if (operator == "x") {
-  calcDisplay.innerHTML = Number(firstNum) * Number(secondNum);
+  calcDisplay.innerHTML = parseFloat(firstNum) * parseFloat(secondNum);
  } else if (operator == "/") {
-  calcDisplay.innerHTML = Number(firstNum) / Number(secondNum);
- } else {
-   calcDisplay.innerHTML = "";
+  calcDisplay.innerHTML = parseFloat(firstNum) / parseFloat(secondNum);
+ } else { 
+  calcDisplay.innerHTML = "";
  }
 }) 
 
+// function for clear
 clearKey.addEventListener("click", () => {
   calcDisplay.innerHTML = "";
+  firstNum = 0;
+  secondNum = 0;
 })
-
-
-
